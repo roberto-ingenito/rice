821a9968d9964c60eb89028465b92e86d07d053e
roberto ingenito <ingenitoroby@gmail.com>
2025-01-06 03:23:00 +0100

    add new configuration

---
+++ install.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Update system
+sudo pacman -Syu
+
+# Install essential packages 
+sudo pacman -S --needed --noconfirm \
+    git \
+    base-devel \
+    xorg xorg-xinit \
+    bspwm sxhkd \
+    alsa-utils pulseaudio pulseaudio-alsa pavucontrol \
+    networkmanager \
+    polybar \
+    picom \
+    dunst \
+    thunar gvfs \
+    brightnessctl \
+    ttf-nerd-fonts-symbols \
+    wget \
+    feh
+
+sudo systemctl enable NetworkManager
+sudo systemctl start NetworkManager
+
+
+# Install legacy audio driver
+sudo mkdir -p /etc/modprobe.d && sudo cp audio-fix.conf /etc/modprobe.d/
+
+
+# Install yay
+if ! command -v yay &> /dev/null; then
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+    cd ..
+    rm -rf yay
+else
+    echo "yay è già installato, proseguo con il resto dell'installazione..."
+fi
+
+
+# Install AMD GPU drivers
+sudo pacman -S --needed --noconfirm \
+    mesa \
+    lib32-mesa \
+    vulkan-radeon \
+    lib32-vulkan-radeon \
+    libva-mesa-driver \
+    libva-utils
+
+
+# Install build tools
+sudo pacman -S --needed --noconfirm \
+    cmake \
+    ninja \
+    meson
+
+
+# Install additional packages
+sudo pacman -S --needed --noconfirm \
+    github-cli \
+    power-profiles-daemon \
+    nodejs npm 
+
+
+cp -rf configs/* ~/.config/
+
+git config --global user.email "ingenitoroby@gmail.com"
+git config --global user.name "Roberto Ingenito"
+
+echo "Installazione completata."
