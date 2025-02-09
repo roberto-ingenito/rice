2e9c4782a9a59864d4a4ff4c93a10a3b4898a515
Roberto Ingenito <ingenitoroby@gmail.com>
2025-02-10 00:21:51 +0100

    .

--- kde/install.sh
+++ kde/install.sh
@@ -85,7 +85,7 @@ sudo pacman -S --needed --noconfirm xorg-xprop xdotool
 mkdir -p ~/.config/autostart
 cp remove_panels_shadow.sh ~/.config/autostart
 echo -e "[Desktop Entry]
-Exec=$HOME/.config/remove_panels_shadow.sh
+Exec=$HOME/.config/autostart/remove_panels_shadow.sh
 Icon=application-x-shellscript
 Name=remove_panels_shadow.sh
 Type=Application
