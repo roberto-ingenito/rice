5d793dace90a7ad791e5ed0d0c0e8e51af29e3b0
Roberto Ingenito <ingenitoroby@gmail.com>
2025-09-20 19:41:05 +0200

    Update install.sh

--- mint/install.sh
+++ mint/install.sh
@@ -149,6 +149,7 @@ install_docker() {
       
     sudo apt-get update
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo usermod -aG docker $USER
     success "Docker installato."
 }
 
